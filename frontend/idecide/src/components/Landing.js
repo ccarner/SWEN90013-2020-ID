bf395c86cc2f3e6684324ae4d724035611693e86
BSoW <954119351@qq.com>
2020-05-18 00:24:08 +1000

    Set up the home page

--- frontend/idecide/src/components/Landing.js
+++ frontend/idecide/src/components/Landing.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+
 
 class Landing extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="jumbotron mt-5">
-          <div className="col-sm-8 mx-auto">
-            <h1 className="text-center">WELCOME</h1>
-          </div>
-        </div>
+      <div className="container" style={{ padding: "150px" }}>
+
+
+        <h1 style={{ color: "purple" }}>Women's Wellbeing Project</h1>
+        <br></br>
+        <h2>Do you worry about whether your relationship is healthy?</h2>
+        <h2>Do you sometimes wonder if you are safe?</h2>
+        <p>This website is for women who feel unsafe or afraid of a current or ex-partner.</p>
+        <button >Start(anonmously)</button><br></br>
+
+        <button >Log in</button>
+
       </div>
     )
   }
