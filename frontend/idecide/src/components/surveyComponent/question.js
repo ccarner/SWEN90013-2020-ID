0d2a2e6fd4a465ac59ad103b74e8e128b394b209
aalsaqer <aalsaqer@student.unimelb.edu.au>
2020-08-11 21:20:06 +1000

    improved Raido buttons in Relationship survey

--- frontend/idecide/src/components/surveyComponent/question.js
+++ frontend/idecide/src/components/surveyComponent/question.js
@@ -1,62 +1,90 @@
 import React from "react";
-
+import { MDBBtn, MDBInput, MDBContainer } from "mdbreact";
 
 export default class Question extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-        };
-    }
+  handleQuestion = (event) => {
+    // the following call will stop the form from submitting
+    event.preventDefault();
 
-    handleQuestion = (event) => {
-        // the following call will stop the form from submitting
-        event.preventDefault();
+    // get the data from the form
+    const dataForm = new FormData(event.target);
+    var formObject = {};
+    dataForm.forEach((value, key) => {
+      formObject[key] = value;
+    });
 
-        // get the data from the form
-        const dataForm = new FormData(event.target);
-        var formObject = {};
-        dataForm.forEach((value, key) => { formObject[key] = value; });
+    this.props.handleQuestion(formObject);
+  };
 
-        this.props.handleQuestion(formObject);
-    }
+  render() {
+    const question = this.props.question;
+    const options = question.selectionOptions;
+    return (
+      <div>
+        <form onSubmit={this.handleQuestion}>
+          <div style={{ color: "purple" }}>{question.questionText}</div>
 
-
-    render() {
-        const question = this.props.question;
-        const options = question.selectionOptions;
-        return (
-            <div>
-                <form onSubmit={this.handleQuestion}>
-                    <div style={{ color: "purple" }}>
-                        {question.questionText}
-                    </div>
-
-                    {question.questionType === "slider" ?
-                        <div className="sliderContainer">
-                            <input className="slider" name="option" type="range" min="1" max="10" />
-                        </div>
-                        : question.questionType === "singleSelection" ?
-                            <div onClick={this.questionHandler}>
-                                <label htmlFor="option1">{options[0]}:</label>
-                                <input type="radio" name="questionResult" value="option1" /><br />
-                                <label htmlFor="option2">{options[1]}:</label>
-                                <input type="radio" name="questionResult" value="option2" /><br />
-                                <label htmlFor="option3">{options[2]}:</label>
-                                <input type="radio" name="questionResult" value="option3" /><br />
-                                <label htmlFor="option4">{options[3]}:</label>
-                                <input type="radio" name="questionResult" value="option4" /><br />
-                                <label htmlFor="option5">{options[4]}:</label>
-                                <input type="radio" name="questionResult" value="option5" /><br />
-                                {/* <br /><button >Rather not to say</button><br /> */}
-                            </div>
-                            :
-                            "Unrecognized Question Type"
-                    }
-                    <button>NEXT</button>
-                </form>
+          {question.questionType === "slider" ? (
+            <div className="sliderContainer">
+              <input
+                className="slider"
+                name="option"
+                type="range"
+                min="1"
+                max="10"
+              />
             </div>
-        );
+          ) : question.questionType === "singleSelection" ? (
+            <div onClick={this.questionHandler} className="font-of-input-box">
+              <form className="f-display">
+                <div className="label">
+                  <label hlabel htmlFor="option0">
+                    <input type="radio" value="option1" unchecked={true} />
+                    {options[0]}
+                  </label>
+                </div>
+                <div className="radio">
+                  <label hlabel htmlFor="option2">
+                    <input type="radio" value="option2" unchecked={true} />
+                    {options[1]}:
+                  </label>
+                </div>
 
-    }
+                <div className="radio">
+                  <label hlabel htmlFor="option3">
+                    <input type="radio" value="option3" unchecked={true} />
+                    {options[2]}:
+                  </label>
+                </div>
+
+                <div className="radio">
+                  <label hlabel htmlFor="option4">
+                    <input type="radio" value="option4" unchecked={true} />
+                    {options[3]}:
+                  </label>
+                </div>
+
+                <div className="radio">
+                  <label hlabel htmlFor="option5">
+                    <input type="radio" value="option5" unchecked={true} />
+                    {options[4]}:
+                  </label>
+                </div>
+              </form>
+
+              {/* <br /><button >Rather not to say</button><br /> */}
+            </div>
+          ) : (
+            "Unrecognized Question Type"
+          )}
+          <button>NEXT</button>
+        </form>
+      </div>
+    );
+  }
 }
