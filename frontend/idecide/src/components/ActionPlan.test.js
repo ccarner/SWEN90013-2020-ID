7fbc633059796978b19782e84249e565e3e3ab74
haobei98 <haobei98@users.noreply.github.com>
2020-08-14 16:44:24 +1000

    Updated testing for recommended strategies

--- frontend/idecide/src/components/ActionPlan.test.js
+++ frontend/idecide/src/components/ActionPlan.test.js
@@ -31,6 +31,31 @@ test('renders recommendation 5', () => {
   const textElement = getByText(/recommendation 1/i);
   expect(textElement).toBeInTheDocument();
 });
+test('renders button 1', () => {
+  const { getByText } = render(<ActionPlan />);
+  const buttonElement = getByText(/View1/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+test('renders button 2', () => {
+  const { getByText } = render(<ActionPlan />);
+  const buttonElement = getByText(/View2/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+test('renders button 3', () => {
+  const { getByText } = render(<ActionPlan />);
+  const buttonElement = getByText(/View3/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+test('renders button 4', () => {
+  const { getByText } = render(<ActionPlan />);
+  const buttonElement = getByText(/View4/i);
+  expect(buttonElement).toBeInTheDocument();
+});
+test('renders button 5', () => {
+  const { getByText } = render(<ActionPlan />);
+  const buttonElement = getByText(/View5/i);
+  expect(buttonElement).toBeInTheDocument();
+});
 
 
 //Test example
