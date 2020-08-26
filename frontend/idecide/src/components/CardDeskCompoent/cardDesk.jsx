84c7c9bf4a240412d64550370357bb3a1403420f
Hao <954119351@qq.com>
2020-08-27 08:54:48 +1000

    Interating the Card Dekker component with the main survey components

--- frontend/idecide/src/components/CardDeskCompoent/cardDesk.jsx
+++ frontend/idecide/src/components/CardDeskCompoent/cardDesk.jsx
@@ -3,18 +3,30 @@ import { Slider } from "antd";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import "./cards.css";
 import "antd/dist/antd.css";
-import questions from "./testdata.js";
+import questions2 from "./testdata.js";
+import { getSurveyById, postingSurvey } from "../../API/surveyAPI";
+import { Spinner, Button } from "react-bootstrap";
 
 export default class CardDesk extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      len: questions.length, // question lenght
-      questions: questions,
+
+      questions: null,
       fadeAwayState: false,
       skiped: { name: "skpied" },
     };
   }
+
+
+  componentDidMount() {
+
+    this.setState({
+      questions: this.props.section.questions
+    });
+  }
+
+
   handleClick(item) {
     const _this = this;
     const questions = this.state.questions;
@@ -32,8 +44,10 @@ export default class CardDesk extends Component {
 
   handleResult(item, result) {
     //here will handle the result !
-    console.log(item.questionId + " " + result);
+    console.log(item.questionId, result, 889);
+    this.props.handleQuestion(item.questionId, result);
     this.handleClick(item);
+    // this.props.handleNav(1);
   }
 
   questionTypeController(item) {
@@ -41,27 +55,36 @@ export default class CardDesk extends Component {
       return (
         <div className="questionContainer">
           <div className="composite-scale-container">
-            <div className="option-container">
+            <div className="option-container"
+            // onChange={(e) =>
+            //   this.props.handleQuestion(1, 6)
+            // }
+            >
               {item.selectionOptions.map((option, index) => (
                 <button
                   key={index}
-                  onClick={() => this.handleResult(item, option.name)}
+                  onClick={() => this.handleResult(item, option)}
                   className="composite-option-button"
                 >
                   <span className="composite-circle top"></span>
-                  <span className="composite-label bottom">{option.name}</span>
+                  <span className="composite-label bottom">{option}</span>
                 </button>
               ))}
             </div>
           </div>
-          <div className="button-container">
+
+
+
+
+
+          {/* <div className="button-container">
             <button
               className="btn"
               onClick={() => this.handleResult(item, this.state.skiped)}
             >
               i rather not anser
             </button>
-          </div>
+          </div> */}
         </div>
       );
     } else if (item.questionType == "slider") {
@@ -80,13 +103,16 @@ export default class CardDesk extends Component {
             }}
           />
 
-          <div className="label-container">
+          {/* Need to discuss the format of the slider */}
+          {/* <div className="label-container">
             {item.labellist.map((v, t) => (
               <span className="label" key={v.index}>
                 {v.name}
               </span>
             ))}
-          </div>
+          </div> */}
+
+          {/* Need to discuss about the button locations */}
           <div className="button-container">
             <button
               className="btn"
@@ -103,13 +129,58 @@ export default class CardDesk extends Component {
           </div>
         </div>
       );
+    } else {
+      return (
+        <div className="questionContainer">
+          Error! Question type not supported!!!
+          <Button
+            className={"purple-gradient"}
+            onClick={(e) => {
+              this.handleSections(-1);
+            }}
+          >
+            {"< Previous"}
+          </Button>
+          <Button
+            className={"purple-gradient"}
+            onClick={(e) => {
+              this.handleSections(1);
+            }}
+          >
+            {"Next Section>"}
+          </Button>
+        </div>
+      );
     }
   }
 
+  handleSections = async (direction) => {
+    console.log(772, this.props.section.questions)
+    await this.props.handleNav(direction);
+    console.log(773, this.props.section.questions)
+    this.setState({
+      questions: this.props.section.questions
+    });
+
+  }
+
+
+
+
   render() {
-    const questionLen = this.state.len;
-    const questions = this.state.questions;
+    // console.log(883, this.props.section)
+    var questions = this.state.questions;
+    // var questions = this.props.section.questions;
+
+
     let fadeAwayState = this.state.fadeAwayState;
+    if (questions == null) {
+      return (
+        <div className="cards-container cards-container-checkbox">
+          Loading...
+        </div>
+      );
+    }
 
     const ItemList = (
       <TransitionGroup>
@@ -128,11 +199,16 @@ export default class CardDesk extends Component {
             >
               <div className="counter">
                 <h5 className="current">{item.questionId}</h5>
-                <h5>/{questionLen}</h5>
+                <h5>/{questions.length + parseInt(item.questionId) - 1}</h5>
               </div>
 
               <h4 className="primary-card-text">{item.questionText}</h4>
+              {/* {console.log(773, item)} */}
               {this.questionTypeController(item)}
+
+
+
+
             </div>
           </CSSTransition>
         ))}
@@ -144,7 +220,25 @@ export default class CardDesk extends Component {
         <div className="cards-wrapper">
           <ul className="cards-list">
             {ItemList}
-            <div>Thanks for completing this exercise.</div>
+            <div>Thanks for completing this section.</div>
+            <div>
+              <Button
+                className={"purple-gradient"}
+                onClick={(e) => {
+                  this.handleSections(-1);
+                }}
+              >
+                {"< Previous"}
+              </Button>
+              <Button
+                className={"purple-gradient"}
+                onClick={(e) => {
+                  this.handleSections(1);
+                }}
+              >
+                {"Next Section>"}
+              </Button>
+            </div>
           </ul>
         </div>
       </div>
