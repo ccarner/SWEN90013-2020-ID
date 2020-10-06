f520b3ff7e24db7615bc03ecc38d3de9f086daa4
Olivia0012 <lu.wang4@student.unimelb.edu.au>
2020-10-07 03:23:17 +1100

    Changed navbar and routes

--- frontend/idecide/src/Layout/Navbar.js
+++ frontend/idecide/src/Layout/Navbar.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import clsx from 'clsx';
-import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch, NavLink } from 'react-router-dom';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -24,9 +24,12 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Collapse from '@material-ui/core/Collapse';
-import MailIcon from '@material-ui/icons/Mail';
+import MenuList from '@material-ui/core/MenuList';
+import MenuItem from '@material-ui/core/MenuItem';
+import StorageIcon from '@material-ui/icons/Storage';
 import ExpandLess from '@material-ui/icons/ExpandLess';
 import ExpandMore from '@material-ui/icons/ExpandMore';
+import BallotIcon from '@material-ui/icons/Ballot';
 import IconLogo from '../images/idecide-logo.png';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
 import { getAllSurveys, getSectionBySurveyId } from '../API/surveyAPI';
@@ -35,12 +38,30 @@ import AssignmentTurnedInIcon from '@material-ui/icons/AssignmentTurnedIn';
 import Loading from '../components/util/loading';
 import SurveyLayout from '../components/dashBoard/view/survey/SurveyLayout';
 import SurveySection from '../components/dashBoard/view/survey/surveyView/SurveySection';
-import MenuItem from '@material-ui/core/MenuItem';
-import MenuList from '@material-ui/core/MenuList';
-import { NavLink } from 'react-router-dom';
-import { Box } from '@material-ui/core';
+import DCLayout from '../components/dashBoard/view/dataCollection/DCLayout';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { Box, Tooltip } from '@material-ui/core';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import LoginPage from '../components/loginComponent/loginPage';
+import RegisterPage from '../components/loginComponent/registerPage';
+import AdminInfo from '../components/loginComponent/adminInfo';
+import Landing from '../components/Landing';
+import SurveyHome from '../components/surveyComponents/surveyHome';
 
 const drawerWidth = 240;
+
+const theme = createMuiTheme({
+	palette: {
+		primary: {
+			main: purple[500]
+		},
+		secondary: {
+			main: '#f44336'
+		}
+	}
+});
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -97,19 +118,21 @@ const useStyles = makeStyles((theme) => ({
 	},
 	content: {
 		flexGrow: 1,
-		padding: theme.spacing(3),
+		padding: theme.spacing(1),
 		transition: theme.transitions.create('margin', {
 			easing: theme.transitions.easing.sharp,
 			duration: theme.transitions.duration.leavingScreen
 		}),
-		marginLeft: -drawerWidth
+		marginLeft: -drawerWidth,
+		marginTop: '5%'
 	},
 	contentShift: {
 		transition: theme.transitions.create('margin', {
 			easing: theme.transitions.easing.easeOut,
 			duration: theme.transitions.duration.enteringScreen
 		}),
-		marginLeft: 0
+		marginLeft: 0,
+		marginTop: '5%'
 	},
 	nested: {
 		paddingLeft: theme.spacing(4)
@@ -121,10 +144,12 @@ function NavBar(props) {
 	const theme = useTheme();
 	const [ isLoading, setIsLoading ] = useState(false);
 	const { width } = props;
-	const [ open, setOpen ] = React.useState(true);
+	const [ open, setOpen ] = React.useState(false);
 	const [ openHelp, setHelp ] = React.useState(false);
 	const [ surveys, setSurveys ] = React.useState([]);
-	const [ sections, setSections ] = React.useState();
+	const [ showSurvey, setShowSurvey ] = React.useState(true);
+	const [ showData, setShowData ] = React.useState(true);
+	const [ isAdmin, setAdmin ] = React.useState(true);
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -138,26 +163,6 @@ function NavBar(props) {
 
 		fetchData();
 	}, []);
-
-	console.log(surveys);
-
-	const [ showSection, setShowSection ] = React.useState(
-		surveys &&
-			surveys.map((a, index) => {
-				//	a.sections=Array();
-				return false;
-			})
-	);
-
-	const fetchData = async (index) => {
-		const surveyId = surveys[index].surveyId;
-		const result = await getSectionBySurveyId(surveyId);
-		surveys[index].sections = result.surveySections;
-		const curData = [ ...showSection ];
-		curData[index] = !curData[index];
-		setShowSection(curData);
-		//	window.location.href = './navbar/surveyId=' + surveyId;
-	};
 
 	const handleDrawerOpen = () => {
 		setOpen(true);
@@ -182,46 +187,59 @@ function NavBar(props) {
 					<AppBar
 						position="fixed"
 						style={{ background: 'white' }}
-						//  className={classes.navbar}
 						className={clsx(classes.appBar, {
 							[classes.appBarShift]: open
 						})}
 					>
 						<Toolbar>
-							{props.location.pathname !== '/navbar/surveys' &&
-							props.location.pathname !== '/navbar/surveyId=1306606397995225088' ? (
-								<div />
-							) : (
-								<IconButton
-									color="black"
-									aria-label="open drawer"
-									onClick={handleDrawerOpen}
-									edge="start"
-									className={clsx(classes.menuButton, open && classes.hide)}
-								>
-									<MenuIcon />
-								</IconButton>
-							)}
+							<Collapse in={isAdmin}>
+								<Tooltip title="Dashboard Side Menu.">
+									<IconButton
+										aria-label="open drawer"
+										onClick={handleDrawerOpen}
+										edge="start"
+										className={clsx(classes.menuButton, open && classes.hide)}
+									>
+										<MenuIcon />
+									</IconButton>
+								</Tooltip>
+							</Collapse>
 							<IconButton onClick={() => (window.location.href = '/')}>
 								<img src={IconLogo} alt="IconLogo" style={{ height: 35, marginTop: 0 }} />
 							</IconButton>
 							<Grid container direction="row" justify="flex-end" alignItems="center" spacing={2}>
-								<Grid item>
-									<Button className={clsx(classes.button)} onClick={handleOpen}>
-										{isWidthUp('sm', width) ? 'Get Help' : 'Help'}
-									</Button>
-								</Grid>
-								<Grid item>
-									<Button
-										className={clsx(classes.button)}
-										onClick={() => {
-											localStorage.clear();
-											window.location.href = 'https://www.weather.com.au/';
-										}}
-									>
-										{isWidthUp('sm', width) ? 'Quick Exit' : 'Exit'}
-									</Button>
-								</Grid>
+								<Collapse in={!isAdmin}>
+									<Grid item>
+										<Tooltip title="Get more help.">
+											<Button className={clsx(classes.button)} onClick={handleOpen}>
+												{isWidthUp('sm', width) ? 'Get Help' : 'Help'}
+											</Button>
+										</Tooltip>
+									</Grid>
+									<Grid item>
+										<Tooltip title="Click to quick exit.">
+											<Button
+												className={clsx(classes.button)}
+												onClick={() => {
+													localStorage.clear();
+													window.location.href = 'https://www.weather.com.au/';
+												}}
+											>
+												{isWidthUp('sm', width) ? 'Quick Exit' : 'Exit'}
+											</Button>
+										</Tooltip>
+									</Grid>
+								</Collapse>
+								<Collapse in={isAdmin}>
+									<Grid item>
+										<Button
+											onClick={() => (window.location.href = '/')}
+											className={clsx(classes.button)}
+										>
+											<ExitToAppIcon />{isWidthUp('sm', width) ? 'LOGOUT' :'' }
+										</Button>
+									</Grid>
+								</Collapse>
 							</Grid>
 						</Toolbar>
 					</AppBar>
@@ -240,57 +258,75 @@ function NavBar(props) {
 							</IconButton>
 						</div>
 						<Divider />
-						<Box p={2}>
-							<Typography variant="h6" gutterBottom>
-								DashBoard
-							</Typography>
+
+						<Box p={3}>
+							<Link to={'/navbar/surveys'}>
+								<Typography variant="h6" gutterBottom>
+									DashBoard
+								</Typography>
+							</Link>
+							<Box p={1} />
+							<AccountCircle />Admin
 						</Box>
+
 						<Divider />
 						<List>
-							{surveys &&
-								surveys.map((survey, index) => (
-									<div key={index}>
-										<ListItem
-											button
-											key={index}
-											onClick={() => {
-												fetchData(index);
-												return <Link to={'/navbar/surveyId=' + survey.surveyId} />;
-											}}
-										>
-											<ListItemIcon>
-												<AssignmentIcon />{' '}
-											</ListItemIcon>
-											<ListItemText primary={survey.surveyTitle} />
-											{showSection[index] ? <ExpandLess /> : <ExpandMore />}
-										</ListItem>
-										<Collapse in={showSection[index]} timeout="auto" unmountOnExit>
-											<List key={survey.surveyId}>
-												{typeof survey.sections !== 'undefined' &&
-													survey.sections.length > 0 &&
-													survey.sections.map((section, index) => (
-														<ListItem button key={section} className={classes.nested}>
-															<ListItemIcon>
-																<AssignmentTurnedInIcon />
-															</ListItemIcon>
-															<Link to="/navbar/surveyId=1306606397995225088">
-																<ListItemText primary={section.sectionTitle} />
-															</Link>
-														</ListItem>
-													))}
-											</List>
-										</Collapse>
-									</div>
-								))}
+							<NavLink to={'/navbar/surveys'} onMouseDown={() => setShowSurvey(!showSurvey)}>
+								<ListItem button>
+									<ListItemIcon>
+										<BallotIcon />{' '}
+									</ListItemIcon>
+									<Typography color="textPrimary" gutterBottom variant="body1">
+										Surveys
+									</Typography>
+									<ListItemText />
+									{showSurvey ? <ExpandLess /> : <ExpandMore />}
+								</ListItem>
+							</NavLink>
+							<Collapse in={showSurvey} timeout="auto" unmountOnExit>
+								{surveys &&
+									surveys.map((survey, index) => (
+										<NavLink to={'/navbar/surveyId=' + survey.surveyId}>
+											<MenuItem className={classes.nested}>
+												<ListItemIcon>
+													<AssignmentIcon />
+												</ListItemIcon>
+												<Typography color="textSecondary" gutterBottom variant="body1">
+													{survey.surveyTitle}
+												</Typography>
+											</MenuItem>
+										</NavLink>
+									))}
+							</Collapse>
+							<NavLink to={'/navbar/datacollection'} onMouseDown={() => setShowData(!showData)}>
+								<ListItem>
+									<ListItemIcon>
+										<StorageIcon />
+									</ListItemIcon>
+									<Typography color="textPrimary" gutterBottom variant="body1">
+										Data
+									</Typography>
+								</ListItem>
+							</NavLink>
 						</List>
 					</Drawer>
 					<main
+						key={props.location}
 						className={clsx(classes.content, {
 							[classes.contentShift]: open
 						})}
 					>
-						<Route path="/navbar/surveys" component={SurveyLayout} />
-						<Route path="/navbar/surveyId=:surveyId" component={SurveySection} />
+						<Switch>
+							<Route exact path="/navbar/surveys" component={SurveyLayout} />
+							<Route path="/navbar/surveyId=:surveyId" component={SurveySection} />
+							<Route path="/dashboard/datacollection" component={DCLayout} />
+							<Route path="/surveyComponent/surveyHome" component={SurveyHome} />
+							<Route path="/loginComponent/loginPage" component={LoginPage} />
+							<Route path="/loginComponent/registerPage" component={RegisterPage} />
+							<Route path="/loginComponent/adminInfo" component={AdminInfo} />
+
+							<Route path="/" component={Landing} />
+						</Switch>
 					</main>
 					<Dialog
 						open={openHelp}
