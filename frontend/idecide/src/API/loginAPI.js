1fee1e522a79b5e0b356fb0966cb62ea16160782
Hao <954119351@qq.com>
2020-10-08 20:34:12 +1100

    Update loginAPI.js

--- frontend/idecide/src/API/loginAPI.js
+++ frontend/idecide/src/API/loginAPI.js
@@ -6,48 +6,59 @@ const USER_URL = "https://www.idecide.icu:9012";
 
 export async function registerUser(userIn) {
   const {
+    // partnerGender,
+    // email,
+    // phoneNumber,
+    // postcode,
     username,
-    password,
-    partnerGender,
-    email,
-    phoneNumber,
-    postcode,
+    password
   } = userIn;
 
   const endpoint = USER_URL + `/user`;
 
-  if (!email) {
-    alert("must include a valid email address");
-    return false;
-  }
 
   const result = await axios({
     url: endpoint, // send a request to the library API
     method: "POST", // HTTP POST method
     headers: {
-      "Content-Type": "application/json",
+      "Content-Type": "application/json"
     },
     data: JSON.stringify({
       // partnerGender,
       // phoneNumber,
       // postcode,
+      // email,
       username,
-      password,
-      email,
+      password
     }),
   });
+
+
+  if (result.data.flag) {
+    loginUser({ "username": username, "password": password });
+  }
 
   return result.data;
 }
 
-export function getAllAdmins() {
+export async function getAllAdmins() {
   const endpoint = USER_URL + `/admin/adminList`;
   try {
-    return axios.get(endpoint).then((res) => res.data);
+    const result = await axios({
+      url: endpoint, // send a request to the library API
+      method: "GET", // HTTP GET method
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": localStorage.getItem("token")
+      }
+    });
+    return result.data;
   } catch (e) {
     return e;
   }
 }
+
+
 
 export function getAllUsers() {
   const endpoint = `https://www.idecide.icu:9012/user/userList`;
@@ -60,20 +71,12 @@ export function getAllUsers() {
 
 
 export async function loginUser(userIn) {
-  const { email, password } = userIn;
+  const { username, password } = userIn;
   var endpoint = USER_URL + `/user/login`;
-  if (userIn.email === "ccarner13@gmail.com") {
+  if (username === "ccarner") {
     endpoint = USER_URL + `/admin/login`;
   }
-  
 
-  
-  // check if the email is present
-
-  if (!email) {
-    alert("must include a valid email address");
-    return false;
-  }
 
   const result = await axios({
     url: endpoint, // send a request to the library API
@@ -82,10 +85,32 @@ export async function loginUser(userIn) {
       "Content-Type": "application/json",
     },
     data: JSON.stringify({
-      email,
+      username,
       password,
     }),
   });
 
+  if (result.data.flag) {
+    localStorage.setItem("token", result.data.data.token);
+    localStorage.setItem("userType", result.data.data.roles);
+    localStorage.setItem("userId", result.data.data.id);
+  }
   return result.data;
+}
+
+
+export async function anonymouseUser() {
+  var endpoint = USER_URL + `/user/anonymousLogin`;
+  const result = await axios({
+    url: endpoint, // send a request to the library API
+    method: "GET", // HTTP GET method
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  localStorage.setItem("token", result.data.data.token);
+  localStorage.setItem("userType", "anonymous");
+  localStorage.setItem("userId", result.data.data.id);
+  return result.data.data.id;
 }
